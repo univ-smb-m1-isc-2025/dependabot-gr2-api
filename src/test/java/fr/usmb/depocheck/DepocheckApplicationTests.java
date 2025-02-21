6c68bc5dbe8aefca8c63a3297862d3afd00a0550
Djapamal <al.leonarduzzi@gmail.com>
2025-02-21 16:45:58 +0100

    Add: JUnit5 and a docker-compose for dev environment

--- src/test/java/fr/usmb/depocheck/DepocheckApplicationTests.java
+++ src/test/java/fr/usmb/depocheck/DepocheckApplicationTests.java
@@ -1,5 +1,6 @@
 package fr.usmb.depocheck;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DepocheckApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void testCase() {
+		Integer a = 10;
+		Integer b = 20;
+		try {
+			Integer result = a + b;
+			Assertions.assertEquals(30, result);
+		}catch (Exception e) {
+			Assertions.fail("Test case failed: " + e.getMessage());
+		}
 	}
 
 }
